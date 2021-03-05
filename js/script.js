86649565d412f24640c613b8a6f9d14d6c858555
SkyBurbuja <kingperez.kingvera@gmail.com>
2021-03-04 18:09:37 -0600

    Add toggle reader and remove button functionality and improved presentation

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function book(title,author,pages,read){
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = () => {
+    this.info = (read) => {
         return `${title} by ${author}, ${pages} pages, ${read}`;
     }
 }
@@ -25,7 +25,7 @@ function createNewBook(){
 }
 
 //Dummy books to gauge display
-const theHobbit = new book('The Hobbit','J.R.R Tolkien',295,'not read yet');
+const theHobbit = new book('The Hobbit','J.R.R Tolkien',295,'not read');
 const harryPotterPS = new book('Harry Potter and the Philosphers Stone','J.K Rowling',250,'read');
 
 addBookToLibrary(theHobbit);
@@ -42,30 +42,65 @@ function displayBooks(){
     removeAllChildNodes(bookContainer);
 
 
+
     for(let i = 0; i<myLibrary.length;i++){
         let newBookCard = document.createElement('div');
         newBookCard.setAttribute('id',`book${i}`);
         newBookCard.setAttribute('class','bookCard');
-        newBookCard.textContent = myLibrary[i].info();
-        createBookRemovalButton(newBookCard);
+        newBookCard.textContent = myLibrary[i].info(myLibrary[i].read);
+        createBookRemovalButton(newBookCard,i);
+        createReadButton(newBookCard,i);
         bookContainer.appendChild(newBookCard);
     }
+
+    const readToggle = document.querySelectorAll('.read');
+    readToggle.forEach(button => button.addEventListener('click',toggleRead));
+    
+    const removal = document.querySelectorAll('.remove');
+    removal.forEach(button => button.addEventListener('click',remove));
 }
 
-function createBookRemovalButton(bookCard){
+function createBookRemovalButton(bookCard,i){
     const newButton = document.createElement('button');
     newButton.textContent = 'Remove';
     newButton.setAttribute('class','remove');
+    newButton.setAttribute('id',`${i}`);
     bookCard.appendChild(newButton);
-}
-
-function remove(e){
     
 }
 
+function createReadButton(bookCard,i){
+    const newButton = document.createElement('button');
+    newButton.textContent = 'Toggle Read';
+    newButton.setAttribute('class','read');
+    newButton.setAttribute('id',`${i}`);
+    bookCard.appendChild(newButton);
+    
+}
+
+
+function remove(e){
+    let dataIndex = parseInt(e.target.id)
+    for(i=0;i<myLibrary.length;i++){
+        if(dataIndex == i){
+            myLibrary.splice(i,1);
+        }
+    }
+    displayBooks();
+}
+
+function toggleRead(e){
+    let dataIndex = parseInt(e.target.id);
+
+    if(myLibrary[dataIndex].read=='read'){
+        myLibrary[dataIndex].read = 'not read';
+    }else if(myLibrary[dataIndex].read =='not read'){
+        myLibrary[dataIndex].read = 'read';
+    }
+    
+    displayBooks();
+}
 const createNewBookBtn = document.querySelector('#addBook');
 createNewBookBtn.addEventListener('click',createNewBook);
 
-const removal = document.querySelectorAll('.remove');
-removal.forEach(button => button.addEventListener('click',remove));
 displayBooks();
